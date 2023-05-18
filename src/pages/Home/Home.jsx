95ab5d620770b92445b5c7a6517aa4c8073c4a29
AbiR7171 <abiremtiag@gmail.com>
2023-05-18 12:18:59 +0600

    AuthProvider with auth context created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Shoptabs from './Shoptabs';
 
 const Home = () => {
     return (
         <div className='mt-10'>
             <Banner/>
+            <Shoptabs/>
         </div>
     );
 };
---
+++ src/pages/Home/Shoptabs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shoptabs = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Shoptabs;
