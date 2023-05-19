1ac5fc625ae1cba06caff73b90d0709f2efb87a4
AbiR7171 <abiremtiag@gmail.com>
2023-05-19 15:41:26 +0600

    single toy added

--- src/pages/Toys/MyToys.jsx
+++ src/pages/Toys/MyToys.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
 
 const MyToys = () => {
+  
+    const {user}=useContext(AuthContext)
+
+
+   const[myToys, setMyToys]=useState([])
+   
+   const url = `https://toy-center-server.vercel.app/mytoyes?email=${user?.email}`
+    useEffect(()=>{
+            fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    },[])
     return (
         <div>
             
